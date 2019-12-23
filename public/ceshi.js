133e15c27c75bca4943a38fd7e2c07dd2c93c5b3
李哲 <lizhe21@maoyan.com>
2019-12-23 20:29:12 +0800

    增加几个demo

---
+++ public/ceshi.js
@@ -0,0 +1,53 @@
+// 不要使用逻辑运算符代替控制流程
+
+// 正例
+const isRunning = true;
+
+function run() {
+}
+if (!isRunning) {
+  run();
+}
+
+// 反例
+const isDancing = true;
+
+function dance() {
+}
+!isDancing && dance();
+
+// 正例
+const isPlaying = true;
+
+function play() {
+}
+if (!isPlaying && play()) {
+  alert(1);
+}
+
+// 反例
+const isSwimming = true;
+
+function swim() {
+}
+function result() {
+  return (!isSwimming && swim());
+}
+result();
+
+// 反例
+let x;
+
+false || (x = 1);
+
+alert(x);
+
+// 正例
+const node = {
+  id: {
+    name: 'ee',
+  },
+};
+const className = node.id && node.id.name;
+
+
