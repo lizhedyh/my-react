4fd33aec4f27c75116d200824b1afdb265f6ce21
李哲 <lizhe21@maoyan.com>
2022-12-27 22:21:30 +0800

    demo移动到component文件夹，简化index代码

---
+++ src/components/copyreact/index.jsx
@@ -0,0 +1,50 @@
+/**
+ * 使用方式
+ * import ReactDOM from './source/kcreate-dom';
+ * import { Copyreact } from './components/copyreact/index';
+ * ReactDOM.render(Copyreact, document.getElementById('root'));
+ */
+import React, { Component } from '../../source/kcreact';
+
+
+function Comp(props) {
+  return <h2>hi {props.name}</h2>
+}
+
+class Comp2 extends Component {
+  render() {
+    return (
+      <div>
+        <h2>hi, {this.props.name}</h2>
+      </div>
+    )
+  }
+}
+
+const users = [{
+  name: '土拨鼠1号',
+  age: 27,
+}, {
+  name: '番薯大神',
+  age: 25,
+}];
+
+const Copyreact = (
+  <div id="demo" style={{ color: "red", border: "1px solid green" }}>
+    <button onClick={() => {
+      alert('clicked');
+    }}>click me</button>
+    <Comp name="函数组件"></Comp>
+    <Comp2 name="类组件"></Comp2>
+    <ul>
+      {
+        users.map((user) => (
+          <li key={user.name}>{user.name}{user.age}</li>
+        ))
+      }
+    </ul>
+  </div>
+)
+
+
+export { Copyreact };
--- src/index.jsx
+++ src/index.jsx
@@ -1,43 +1,4 @@
-import React, { Component } from './source/kcreact';
-import ReactDOM from './source/kcreate-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function Comp(props) {
-  return <h2>hi {props.name}</h2>
-}
-
-class Comp2 extends Component {
-  render() {
-    return (
-      <div>
-        <h2>hi, {this.props.name}</h2>
-      </div>
-    )
-  }
-}
-
-const users = [{
-  name: '土拨鼠1号',
-  age: 27,
-}, {
-  name: '番薯大神',
-  age: 25,
-}];
-
-const jsx = (
-  <div id="demo" style={{ color: "red", border: "1px solid green" }}>
-    <button onClick={() => {
-      alert('clicked');
-    }}>click me</button>
-    <Comp name="函数组件"></Comp>
-    <Comp2 name="类组件"></Comp2>
-    <ul>
-      {
-        users.map((user) => (
-          <li key={user.name}>{user.name}{user.age}</li>
-        ))
-      }
-    </ul>
-  </div>
-)
-
-ReactDOM.render(jsx, document.getElementById('root'));
+ReactDOM.render(<div>Hello World</div>, document.getElementById('root'));
