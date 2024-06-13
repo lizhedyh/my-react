0b223b25d1e2657fd01dd0c17443406e5b6b5f1f
lizhedyh <zhe.li@fuseignited.com>
2024-06-13 23:06:17 +0800

    simple use of htmlWebpackPlugin

--- config/webpack.config.simple.js
+++ config/webpack.config.simple.js
@@ -1,16 +1,25 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 const appDirectory = fs.realpathSync(process.cwd()); // get absolute path for process startup
 
 module.exports = {
   mode: "development",
-  context: path.resolve(appDirectory, 'src/mock'),
+  context: path.resolve(appDirectory, "src/mock"),
   entry: {
-    index: './index.js',
-    hello: './hello.js',
+    index: "./index.js",
+    hello: "./hello.js",
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(appDirectory, 'dist')},
-}
+    filename: "[name].js",
+    path: path.resolve(appDirectory, "dist"),
+    clean: true, // clear dist before build
+  },
+  plugins: [new htmlWebpackPlugin({ title: "xx" })],
+  optimization: {
+    runtimeChunk: {
+      name: "runtime", // 运行时 chunk 的名字
+    },
+  },
+};
